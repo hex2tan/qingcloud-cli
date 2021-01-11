3f00307fb08ab0120a412eb6bd68ef28fb2da7a3
hex2tan <tianjingke@gmail.com>
2021-01-11 22:22:50 +0800

    add config.go

--- cmd/config.go
+++ cmd/config.go
@@ -1,1 +1,15 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var echoDemoCmd = &cobra.Command{
+	Use:   "echo-demo-config",
+	Short: "echo demo configuration to standard output",
+	Long: "qingcloud-cli echo-demo-config > $HOME/.qingcloud.yaml",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("qy_access_key_id: 'QYACCESSKEYIDEXAMPLE'\nqy_secret_access_key: 'SECRETACCESSKEY'\nzone: 'pek3'\n\n")
+	},
+}
